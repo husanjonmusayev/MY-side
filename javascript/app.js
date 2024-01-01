af2f8a70005a74baaf562238dfa38e48f5fe279c
husanjonmusayev <husanjonm279@gmail.com>
2024-01-01 18:40:55 +0500

    uy ishi

---
+++ javascript/app.js
@@ -0,0 +1,46 @@
+let item = document.querySelectorAll('.slider .list .item');
+let nex = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thum = document.querySelectorAll('.thumbunali .item');
+let countItem = item.length;
+let itemActive = 0;
+
+nex.onclick = function () {
+    itemActive = itemActive + 1;
+    if (itemActive >= countItem) {
+        itemActive = 0;
+    }
+showSlider() 
+};
+prev.onclick = function (){
+itemActive = itemActive - 1;
+if(itemActive < 0){
+    itemActive = countItem - 1;
+};
+
+showSlider()
+
+};
+
+function showSlider(){
+let itemActiveOld = document.querySelector('.slider .list .item.active')
+let thumActivOld = document.querySelector('.thumbunali .item.active')
+itemActiveOld.classList.remove('active')
+thumActivOld.classList.remove('active')
+
+
+item[itemActive].classList.add('active')
+thum[itemActive].classList.add('active')
+
+}
+
+thum.forEach((thum , index) =>{
+    thum.addEventListener('click', () => {
+       itemActive = index
+       showSlider();
+    }) 
+});
+
+let refreshInterval = setInterval(() =>{
+nex.click();
+}, 6000)
